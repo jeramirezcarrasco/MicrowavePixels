099f4bd04a844a92d3c6b5f6a548ed8faee96dbe
Eric Grochowicz <ericgrochowicz@hotmail.com>
2019-07-17 14:28:10 -0300

    Thin Platform that you only collide with from above functionality in 'Jungle's Playground'

---
+++ MicrowavePixels2019s/Assets/Scripts/ThinPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThinPlatform : MonoBehaviour
+{
+	private GameObject player;
+	private BoxCollider2D col;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (player.transform.position.y >= transform.position.y)
+		{
+			if (gameObject.layer != 9)
+			{
+				gameObject.layer = 9;
+			}
+		}
+		else
+		{
+			if (gameObject.layer != 0)
+			{
+				gameObject.layer = 0;
+			}
+		}
+    }
+}
