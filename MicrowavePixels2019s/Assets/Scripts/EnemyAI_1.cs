eae89c0ca9f2ad359061f74d1705173f1025f0ea
aallen170 <aallen170@gmail.com>
2019-07-20 21:42:30 -0500

    update

--- MicrowavePixels2019s/Assets/Scripts/EnemyAI_1.cs
+++ MicrowavePixels2019s/Assets/Scripts/EnemyAI_1.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class EnemyAI_1 : MonoBehaviour
 {
+    public LayerMask obstacleMask;
 
     public float Maxspeed;
     public float DownView;
     float Speed;
     public float Acceleration;
 
-    private bool MovRight = true;
+    private bool MovRight = true, hitWall = false;
     public int AlertViewRange;
-
 
     public Transform GroundDetection1;
     public Transform GroundDetection2;
@@ -21,8 +21,6 @@ public class EnemyAI_1 : MonoBehaviour
     private ChaseAI chaseAI;
     private LineOfSightVisual lineOfSightVisual;
     private SpriteRenderer mySpriteRenderer;
-
-
 
     private void Awake()
     {
@@ -65,9 +63,12 @@ public class EnemyAI_1 : MonoBehaviour
         transform.Translate(Vector2.right * Speed * Time.deltaTime);
         RaycastHit2D groundSlowDown = Physics2D.Raycast(GroundDetection2.position, Vector2.down, DownView);
         RaycastHit2D ground = Physics2D.Raycast(GroundDetection1.position, Vector2.down, DownView);
+
+        DetectWall();
+
         if (groundSlowDown.collider == false)
         {
-            if (Speed > (Maxspeed/3))
+            if (Speed > (Maxspeed / 3))
             {
                 Speed -= Acceleration;
             }
@@ -79,18 +80,18 @@ public class EnemyAI_1 : MonoBehaviour
                 Speed += Acceleration;
             }
         }
-        if (ground.collider == false)
+        if (ground.collider == false || hitWall)
         {
             if (MovRight)
             {
                 transform.eulerAngles = new Vector3(0, 0, 180);
-
+                mySpriteRenderer.flipY = !mySpriteRenderer.flipY;
                 MovRight = false;
-                
             }
             else
             {
                 transform.eulerAngles = new Vector3(0, 0, 0);
+                mySpriteRenderer.flipY = !mySpriteRenderer.flipY;
                 MovRight = true;
 
             }
@@ -98,7 +99,28 @@ public class EnemyAI_1 : MonoBehaviour
         }
     }
 
-   
+    private void DetectWall()
+    {
+        RaycastHit2D wallHit = Physics2D.Raycast (
+            new Vector2 (
+                MovRight ?
+                    GetComponent<BoxCollider2D>().bounds.max.x
+                :
+                    GetComponent<BoxCollider2D>().bounds.min.x,
+                GetComponent<BoxCollider2D>().bounds.center.y
+            ),
+            MovRight ? Vector2.right : Vector2.left,
+            Mathf.Infinity,
+            obstacleMask
+        );
+
+        if (wallHit)
+        {
+            if (wallHit.distance == 0) hitWall = true;
+            else hitWall = false;
+        }
+    }
+
 }
 
     
