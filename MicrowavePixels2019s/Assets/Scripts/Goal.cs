4e1111a3e82afd23b3092e389ad8066ed1dd638c
Grochowicz <ericgrochowicz@hotmail.com>
2019-07-23 14:02:17 -0300

    Goal takes you to the next scene now

--- MicrowavePixels2019s/Assets/Scripts/Goal.cs
+++ MicrowavePixels2019s/Assets/Scripts/Goal.cs
@@ -26,7 +26,7 @@ public class Goal : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (LandedOnGoal()) /*LoadNextLevel()*/ print("in goal");
+        if (LandedOnGoal()) LoadNextLevel();
     }
 
     private void LoadNextLevel()
