c451bc7a93906d460690f3202ab3f245cb77ebc7
Grochowicz <ericgrochowicz@hotmail.com>
2019-07-23 16:12:37 -0300

    Credits update + minor change in Stan's level

--- MicrowavePixels2019s/Assets/Scripts/ScrollCredits.cs
+++ MicrowavePixels2019s/Assets/Scripts/ScrollCredits.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ScrollCredits : MonoBehaviour
 {
 	[SerializeField] float scrollSpeed;
+	[SerializeField] float yCreditsEnd;
 
     // Update is called once per frame
     void FixedUpdate()
     {
 		transform.Translate(new Vector3(0, scrollSpeed, 0));
+		print(GetComponent<RectTransform>().position.y);
+		if (GetComponent<RectTransform>().position.y > yCreditsEnd) SceneManager.LoadScene(0);
     }
 }
