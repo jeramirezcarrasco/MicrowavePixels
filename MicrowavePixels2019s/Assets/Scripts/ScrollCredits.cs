e4d2dec8de747358d3c4ce86a53bca1bf8e7b6c1
Grochowicz <ericgrochowicz@hotmail.com>
2019-07-23 15:13:27 -0300

    Credits scene

---
+++ MicrowavePixels2019s/Assets/Scripts/ScrollCredits.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollCredits : MonoBehaviour
+{
+	[SerializeField] float scrollSpeed;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+		transform.Translate(new Vector3(0, scrollSpeed, 0));
+    }
+}
